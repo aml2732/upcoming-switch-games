0dacfb076b0170549e2b6077398400c85cee764b
unknown <aml2732@utexas.edu>
2022-07-24 23:03:54 -0500

    Tile styling perfected

--- src/Tile.js
+++ src/Tile.js
@@ -4,7 +4,10 @@ function Tile({data}) {
   console.log('data.id', data.id)
   return (
     <div key={`tile-${data.id}`} className="tile" style={{backgroundImage:  `linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.20)), url('/games/image/${data.id}')`}}>
-
+      <div className="hidden-till-tile-flipped">
+        <a href={data.linkto} target="_blank">More info</a>
+        <p>{data.description}</p>
+      </div>
       <div className="title-text-container">{data.name}</div>
     </div>
   );
