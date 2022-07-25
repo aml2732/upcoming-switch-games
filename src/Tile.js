6744a254f71f1e041e9e169c8982886c42ae685e
unknown <aml2732@utexas.edu>
2022-07-24 20:44:59 -0500

    Tile style changes

--- src/Tile.js
+++ src/Tile.js
@@ -3,7 +3,7 @@ import './Tile.css'
 function Tile({data}) {
   console.log('data.id', data.id)
   return (
-    <div key={`tile-${data.id}`} className="tile" style={{backgroundImage:  `linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.60)), url('/games/image/${data.id}')`}}>
+    <div key={`tile-${data.id}`} className="tile" style={{backgroundImage:  `linear-gradient(to bottom, transparent, rgba(0, 0, 0, 0.20)), url('/games/image/${data.id}')`}}>
 
       <div className="title-text-container">{data.name}</div>
     </div>
